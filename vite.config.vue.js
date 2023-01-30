0c68c40a89e2f72c87b9df72f08a5bc9e4436799
git-saya <eki.ridyanto@gmail.com>
2023-01-30 10:09:28 +0700

    bootcamp inertia react

--- vite.config.vue.js
+++ vite.config.vue.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-        react(),
+        vue(),
         laravel({
-            input: ['resources/js/app.jsx'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true
         }),
     ],
